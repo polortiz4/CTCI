48703800538f3875cd86b25fc7522d77004f1ee6
Pablo Ortiz <pablo.ortiz@duke.edu>
2021-05-22 02:39:02 -0400

    exercise 6.5

---
+++ src/math_and_logic_puzzles/jogs_of_water.rs
@@ -0,0 +1,8 @@
+// Solution to Exercise 6.5
+
+/// How to measure 4q of water with only a 3q and a 5q container?
+///
+/// You fill the 5q one and pour 3q to the 3q one, which you then empty and end up with 2q.
+/// Pass the 2q from the 5q container to the 3q one.
+/// Fill the 5q container and pour 1q to the 3q container (until it fills up)
+/// The 5q container has now 4q of water
