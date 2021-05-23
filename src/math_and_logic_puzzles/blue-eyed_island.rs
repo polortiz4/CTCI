893da11f1b09cc72348e9474d2c06a9b76d90649
Pablo Ortiz <pablo.ortiz@duke.edu>
2021-05-23 14:49:15 -0400

    Exercise 6.7

---
+++ src/math_and_logic_puzzles/blue-eyed_island.rs
@@ -0,0 +1,4 @@
+// Solution to Exercise 6.6
+
+/// Every person boards the plane after n days, where n is the number of blue-eyed people they see.
+/// This means it will take n-1 days for all blue-eyed people to leave.
---
+++ src/math_and_logic_puzzles/the_apocalypse.rs
@@ -0,0 +1,5 @@
+// Solution to Exercise 6.7
+
+/// What is the ratio of male to femal, if all families stop having children after giving birth to a female?
+
+/// 0.5. Nature doesn't change the probability of birthing a boy even if you swtop having kids.
